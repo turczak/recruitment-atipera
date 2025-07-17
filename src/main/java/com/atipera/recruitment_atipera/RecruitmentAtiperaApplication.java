fdbdf8f3dbc5897e7f3296f7ab9635a96752d542
turczak <wojciech.turczak97@gmail.com>
2025-07-17 20:30:25 +0200

    Initial commit

---
+++ src/main/java/com/atipera/recruitment_atipera/RecruitmentAtiperaApplication.java
@@ -0,0 +1,13 @@
+package com.atipera.recruitment_atipera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAtiperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAtiperaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipera/recruitment_atipera/RecruitmentAtiperaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.recruitment_atipera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAtiperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
