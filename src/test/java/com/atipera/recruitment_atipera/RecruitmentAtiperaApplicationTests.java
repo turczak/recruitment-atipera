e6d5ed24b6941971e61291c715a60ad9155977c5
turczak <wojciech.turczak97@gmail.com>
2025-07-18 07:31:51 +0200

    Added happy path test.

--- src/test/java/com/atipera/recruitment_atipera/RecruitmentAtiperaApplicationTests.java
+++ src/test/java/com/atipera/recruitment_atipera/RecruitmentAtiperaApplicationTests.java
@@ -1,13 +1,51 @@
 package com.atipera.recruitment_atipera;
 
+import com.atipera.recruitment_atipera.model.dto.RepositoryDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecruitmentAtiperaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void givenExistingUser_whenGetNonForkRepositories_thenReturnCorrectResult() {
+        // given
+        String username = "turczak";
+        String url = "/api/" + username;
+        // when
+        ResponseEntity<List<RepositoryDto>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<RepositoryDto>>() {
+                }
+        );
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        List<RepositoryDto> repositories = response.getBody();
+        assertThat(repositories).isNotNull().isNotEmpty();
+        repositories.forEach(repo -> {
+            assertThat(repo.name()).isNotBlank();
+            assertThat(repo.ownerLogin()).isNotBlank().isEqualTo(username);
+            assertThat(repo.branches()).isNotNull().isNotEmpty();
+            repo.branches().forEach(branch -> {
+                assertThat(branch.name()).isNotBlank();
+                assertThat(branch.lastCommitSha()).isNotBlank();
+            });
+        });
+    }
 
 }
